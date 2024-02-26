ec3100e4ab3691a556ef9bf2902fb365532d796f
Hannah Ishimwe <hannah.ishimwe@kcl.ac.uk>
2024-02-26 15:34:27 +0000

    Clean things up

--- my_portfolio/src/components/Landing.vue
+++ my_portfolio/src/components/Landing.vue
@@ -1,7 +1,7 @@
 <script setup>
 
-import Button from './Button.vue';
-import Heading from './Heading.vue'
+import Button from './helpers/Button.vue';
+import Heading from './helpers/Heading.vue'
 defineProps({
   copy: String,
 })
@@ -10,14 +10,23 @@ defineProps({
 
 <template>
   <Heading/>
-  <p class="header-copy">{{ copy }}</p>
-  <Button copy="Explore" destination="./About" />
+  <div class="copy-parent">
+    <p class="landing-copy">{{ copy }}</p>
+  </div>
+  <Button copy="Learn More" destination="./About.vue" />
 
 </template>
 
 <style scoped>
   @import url('https://fonts.googleapis.com/css2?family=Questrial&family=Radley:ital@0;1&display=swap');
-  .header-copy {
+
+  .copy-parent{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 3rem;
+}
+  .landing-copy {
     font-family: 'Questrial', serif;
     font-size: 2rem;
     color: #f8eaea;
--- my_portfolio/src/components/helpers/Button.vue
+++ my_portfolio/src/components/helpers/Button.vue
@@ -8,13 +8,20 @@ defineProps({
 </script>
 
 <template>
-<div>
-    <a href={{destination}} class="button">{{ copy }}</a>
+<div class="button-parent">
+    <a href={{destination}} class="button"><em>{{ copy }}</em></a>
 </div>
 </template>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Radley:ital@0;1&display=swap');
+
+.button-parent{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 3rem;
+}
 .button{
     position: relative;
     font-family: 'Radley', serif;
@@ -25,6 +32,7 @@ defineProps({
     cursor: pointer;
     background-color: transparent;
     border: none;
+
 }
 
 .button::after {
@@ -35,7 +43,8 @@ defineProps({
   width: 0;/* Initial width of underline */
   height: 1px;
   background-color: #f8eaea; /* Color of underline */
-  transition: width 0.3s ease; 
+  transition: width 0.5s ease; 
+
 }
 
 .button:hover::after {
