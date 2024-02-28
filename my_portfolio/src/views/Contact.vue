985362c8738dfc0111f46f1db53e38c3e71a62b7
Hannah Ishimwe <hannah.ishimwe@kcl.ac.uk>
2024-02-28 14:16:37 +0000

    Add navbar to other pages

--- my_portfolio/src/views/Contact.vue
+++ my_portfolio/src/views/Contact.vue
@@ -1,8 +1,9 @@
 <script setup>
-
+import NavBar from '../components/NavBar.vue'
 </script>
 
 <template>
+<NavBar/>
 <h1>Contact Page</h1>
 </template>
 
--- my_portfolio/src/views/Experience.vue
+++ my_portfolio/src/views/Experience.vue
@@ -1,8 +1,9 @@
 <script setup>
-
+import NavBar from '../components/NavBar.vue'
 </script>
 
 <template>
+<NavBar/>
 <h1>Experience Page</h1>
 </template>
 
